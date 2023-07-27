e4819417a1d70a818c27ba1b5973e28a6fa7a377
abd <you@example.com>
2023-07-27 23:19:34 +0300

    init

---
+++ doctor/src/main/java/com/microservices/doctor/DoctorApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorApplication.class, args);
+	}
+
+}
